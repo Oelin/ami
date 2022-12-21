cf8c127cf30a70f6ab673209ad161d13d6ed4bf1
Oelin <42823429+Oelin@users.noreply.github.com>
2022-12-21 14:20:27 +0000

    Update langauge-identification.py

--- examples/langauge-identification.py
+++ examples/langauge-identification.py
@@ -6,15 +6,23 @@ An example of fast few-shot document classification using AMI with zlib.
 from ami import match
 
 
-spanish_context = '. '.join([
+train_spanish = '. '.join([
   'este fragmento de texto esta escrito en espanol',
   'el espanol es una lengua romance que se habla en espana y en gran parte de america central y del sur',
   'un gato blanco sentado en la alfombra',
 ])
 
 
-english_examples = '. '.join([
+train_english = '. '.join([
   'this piece of text is written in english',
   'england is a country that is part of the united kingdom',
   'a white cat is sitting on a mat'
 ])
+
+
+def classify(x):
+  return match(train_spanish, train_english, x)
+
+
+classify('pizza is the best food in the world') # returns 1 (English)
+classify('la pizza es la mejor comida del mundo') # returns 0 (Spanish)
