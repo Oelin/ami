5dbf5edf711f2fedbe43e97d6632dd406f1d9ea4
Oelin <42823429+Oelin@users.noreply.github.com>
2022-12-21 13:51:44 +0000

    Update ami.py

--- ami.py
+++ ami.py
@@ -1,15 +1,18 @@
 import zlib
 
 
+encode = zlib.compress
+
+
 def ami(x: str, y: str) -> float:
         """
         Returns the normalized approximate mutual information between
         strings x and y.
         """
 
-        lx = len(zlib.compress(x))
-        ly = len(zlib.compress(y))
-        lyx = len(zlib.compress(y + x))
+        lx = len(encode(x))
+        ly = len(encode(y))
+        lyx = len(encode(y + x))
         lx_y = min(lx, max(0, lyx - ly)) # 0 <= L(x|y) <= L(x)
         ixy = lx - lx_y
 
@@ -22,9 +25,9 @@ def ami2(x: str, y: str) -> float:
         strings x and y.
         """
 
-        lx = len(zlib.compress(x))
-        ly = len(zlib.compress(y))
-        lxy = min(len(zlib.compress(x + y)), len(zlib.compress(y + x)))
+        lx = len(encode(x))
+        ly = len(encode(y))
+        lxy = min(len(encode(x + y)), len(encode(y + x)))
         lxy = min(lx + ly, max(0, lxy))
 
         return ((lx + ly) - lxy) / (lx + ly) * 2
